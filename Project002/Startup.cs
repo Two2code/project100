cbb3248c6651b92a8af3a643c64d15eccb43b6c4
Hong Zheng <hong.zheng@iquate.com>
2018-05-04 15:47:28 +0800

    commit project
    everything

---
+++ Project002/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project002.Startup))]
+namespace Project002
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
